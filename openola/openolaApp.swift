a504d21431bdd514eebcafaea1d07d1f67063ca9
Scott <s@st.im>
2026-03-17 14:10:43 +0000

    Initial Commit

---
+++ openola/openolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  openolaApp.swift
+//  openola
+//
+//  Created by Scott on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct openolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
